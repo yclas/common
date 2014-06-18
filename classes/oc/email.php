62d8ececce700016e35081a056a68211f571aea4
Chema <neo22s@gmail.com>
2014-06-18 12:47:15 +0200

    email common

--- classes/oc/email.php
+++ classes/oc/email.php
@@ -98,4 +98,87 @@ class OC_Email {
  
     }
 
-} //en email
+    /**
+     * sends an email using content from model_content
+     * @param  string $to        
+     * @param  string $to_name   
+     * @param  string $from      
+     * @param  string $from_name 
+     * @param  string $content   seotitle from Model_Content
+     * @param  array $replace   key value to replace at subject and body
+     * @param  array $file      file to attach to email
+     * @return boolean            s
+     */
+    public static function content($to, $to_name='', $from = NULL, $from_name =NULL, $content, $replace, $file=NULL)
+    {
+        
+        $email = Model_Content::get_by_title($content,'email');
+
+        //content found
+        if ($email->loaded())
+        { 
+            if ($replace===NULL) 
+                $replace = array();
+
+            if ($from === NULL)
+                $from = $email->from_email;
+
+            if ($from_name === NULL )
+                $from_name = core::config('general.site_name');
+
+            if (isset($file) AND self::is_valid_file($file))
+                $file_upload = $file;
+            else
+                $file_upload = NULL;
+
+            //adding extra replaces
+            $replace+= array('[SITE.NAME]'      =>  core::config('general.site_name'),
+                             '[SITE.URL]'       =>  core::config('general.base_url'),
+                             '[USER.NAME]'      =>  $to_name);
+
+            if(!is_array($to))
+                $replace += array('[USER.EMAIL]'=>$to);
+
+            //adding anchor tags to any [URL.* match
+            foreach ($replace as $key => $value) 
+            {
+                if(strpos($key, '[URL.')===0 OR $key == '[SITE.URL]'  AND $value!='')
+                    $replace[$key] = '[url='.$value.']'.parse_url($value, PHP_URL_HOST).'[/url]';
+            }
+
+            $subject = str_replace(array_keys($replace), array_values($replace), $email->title);
+            $body    = str_replace(array_keys($replace), array_values($replace), $email->description);
+
+            return Email::send($to,$to_name,$subject,$body,$from,$from_name, $file_upload); 
+
+        }
+        else return FALSE;
+
+    }
+
+    /**
+     * returns true if file is of valid type.
+     * Its used to check file sent to user from advert usercontact
+     * @param array file
+     * @return BOOL 
+     */
+    public static function is_valid_file($file)
+    {
+        //catch file
+        $file = $_FILES['file'];
+        //validate file
+        if( $file !== NULL)
+        {     
+            if ( 
+                ! Upload::valid($file) OR
+                ! Upload::not_empty($file) OR
+                ! Upload::type($file, array('jpg', 'jpeg', 'png', 'pdf','doc','docx')) OR
+                ! Upload::size($file,'3M'))
+                {
+                    return FALSE;
+                }
+            return TRUE;
+        }
+    }
+
+} //end email

e313f526a7ca5c5e0316d104b48fec6f6a38469c
Chema <neo22s@gmail.com>
2014-07-21 17:21:16 +0200

    sitemap

--- views/oc-panel/pages/tools/sitemap.php
+++ views/oc-panel/pages/tools/sitemap.php
@@ -1,9 +1,16 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
 
-
 <div class="page-header">
-	<h1><?=__('Sitemap')?></h1>
-<p><?=__('Last time generated')?> <?=Date::unix2mysql(Sitemap::last_generated_time())?></p>
-<a class="btn btn-primary pull-right ajax-load" href="<?=Route::url('oc-panel',array('controller'=>'tools','action'=>'sitemap'))?>?force=1">
-  <?=__('Generate')?></a>
+    <h1><?=__('Sitemap')?></h1>
 </div>
+
+<p><?=__('Last time generated')?> <?=Date::unix2mysql(Sitemap::last_generated_time())?> <a class="btn btn-primary ajax-load" title="<?=__('Sitemap')?>" href="<?=Route::url('oc-panel',array('controller'=>'tools','action'=>'sitemap'))?>?force=1">
+  <?=__('Generate')?></a><br>
+
+
+
+ <?=__('Your sitemap XML to submit to engines')?>
+<input type="text" value="<?=core::config('general.base_url')?><?=(file_exists(DOCROOT.'sitemap-index.xml'))? 'sitemap-index.xml':'sitemap.xml.gz'?>" />
+
+
+</p>

693361f5ececf6ed910382116897a6b0fd0565b4
Emmanuel <emanwebdev@gmail.com>
2014-09-04 01:00:09 +0200

    Added HTTP headers  for Bots & Crawlers
    y.c.  Notify bots & crawlers to come back in 15 minutes, i.e. 900
    seconds

--- classes/controller/maintenance.php
+++ classes/controller/maintenance.php
@@ -9,6 +9,11 @@ class Controller_Maintenance extends Kohana_Controller {
             HTTP::redirect(Route::url('default'));
 
         $this->response->status(503);
+        // Notify bots & crawlers to consider the site as only temporary unavailable
+        header('HTTP/1.1 503 Service Temporarily Unavailable');
+        header('Status: 503 Service Temporarily Unavailable');
+        // Notify bots & crawlers to come back in 15 minutes, i.e. 900 seconds
+        header('Retry-After: 900');
         $this->response->body(View::factory('pages/error/503')->render());
 	}
 

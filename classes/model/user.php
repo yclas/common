51a5cf2e38545b2d4caf43a7a564e8b520f37fee
chema <neo22s@gmail.com>
2015-02-05 13:11:55 +0100

    nothing

--- classes/model/user.php
+++ classes/model/user.php
@@ -2,10 +2,10 @@
 /**
  * User model
  *
- * @author		Chema <chema@open-classifieds.com>
- * @package		OC
- * @copyright	(c) 2009-2013 Open Classifieds Team
- * @license		GPL v3
+ * @author      Chema <chema@open-classifieds.com>
+ * @package     OC
+ * @copyright   (c) 2009-2013 Open Classifieds Team
+ * @license     GPL v3
  * *
  */
 class Model_User extends Model_OC_User {} // END Model_User

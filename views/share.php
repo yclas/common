972ce5c589ac170a0e8b90d81dedd4e4f88f8c00
chema <neo22s@gmail.com>
2015-03-03 14:01:48 +0100

    closes https://github.com/open-classifieds/openclassifieds2/issues/1136

---
+++ views/share.php
@@ -0,0 +1,12 @@
+<span class='st_whatsapp_large' displayText='WhatsApp'></span>
+<span class='st_facebook_large' displayText='Facebook'></span>
+<span class='st_twitter_large' displayText='Tweet'></span>
+<span class='st_linkedin_large' displayText='LinkedIn'></span>
+<span class='st_pinterest_large' displayText='Pinterest'></span>
+<span class='st_googleplus_large' displayText='Google +'></span>
+<span class='st_email_large' displayText='Email'></span>
+<span class='st_print_large' displayText='Print'></span>
+
+<script type="text/javascript">var switchTo5x=true;</script>
+<script type="text/javascript" src="//ws.sharethis.com/button/buttons.js"></script>
+<script type="text/javascript">stLight.options({publisher: "12d591ee-d8ab-456d-807f-f11fb504d15b", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>

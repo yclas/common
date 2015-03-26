6a58a7e2c0708bead7398b79bc5bdf918a76d76f
Oliver <oliverds@users.noreply.github.com>
2015-03-26 17:59:45 +0100

    Fix nl2br and new function removenl

--- classes/oc/text.php
+++ classes/oc/text.php
@@ -171,7 +171,17 @@ class OC_Text extends Kohana_Text {
      */
     public static function nl2br($var)
     {
-        return str_replace(array("\\r\\n","\r\\n","r\\n","\r\n", "\n", "\r"), '<br />', nl2br($var));
+        return str_replace(array("\\r\\n","\r\\n","r\\n","\r\n", "\n", "\r"), '<br />', $var);
+    }
+
+    /**
+     *
+     * removes line breaks from text
+     * @param string $var
+     */
+    public static function removenl($var)
+    {
+        return str_replace(array("\\r\\n","\r\\n","r\\n","\r\n", "\n", "\r"), '', $var);
     }
 
 }

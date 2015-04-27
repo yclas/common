b026e2abbe90625c4c859d1c62481ec7df3f89db
ConstantinosOC <constantinos@open-classifieds.com>
2015-04-27 09:45:50 +0300

    changed links to guides

--- views/oc-panel/pages/tools/sitemap.php
+++ views/oc-panel/pages/tools/sitemap.php
@@ -2,7 +2,7 @@
 
 <div class="page-header">
 	<h1><?=__('Sitemap')?></h1>
-    <a target='_blank' href='http://docs.yclas.com/sitemap-classifieds-website/'><?=__('Read more')?></a>
+	<a target='_blank' href='http://docs.yclas.com/sitemap-classifieds-website/'><?=__('Read more')?></a>
 </div>
 
 <div class="row">

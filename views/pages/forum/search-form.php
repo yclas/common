cb0024454f015172a4af15842909256c4d551d11
Oliver <oliverds@users.noreply.github.com>
2015-08-13 11:23:47 -0500

    wrap missing strings in a __() function

--- views/pages/forum/search-form.php
+++ views/pages/forum/search-form.php
@@ -1,9 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
 <form class="pull-right" action="<?=Route::URL('forum-home')?>" method="get">
     <div class="pull-right">&nbsp;</div>
-    <button class="btn btn-default pull-right" type="submit" value="<?=_('Search')?>"><?=__('Search')?></button>
+    <button class="btn btn-default pull-right" type="submit" value="<?=__('Search')?>"><?=__('Search')?></button>
     <div class="pull-right">&nbsp;</div>
     <div class="pull-right">
-        <input type="text" class="form-control" id="task-table-filter" data-action="filter" data-filters="#task-table" placeholder="<?=('Search')?>" type="search" value="<?=core::get('search')?>" name="search" />
+        <input type="text" class="form-control" id="task-table-filter" data-action="filter" data-filters="#task-table" placeholder="<?=__('Search')?>" type="search" value="<?=core::get('search')?>" name="search" />
     </div>
 </form>

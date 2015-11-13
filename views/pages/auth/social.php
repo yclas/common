db1eb8c1529fdc1ca2262acf8d132a3829e9482f
Oliver <oliverds@users.noreply.github.com>
2015-11-13 17:07:41 -0600

    Social::enabled_providers() function
    Used to improve social view

--- views/pages/auth/social.php
+++ views/pages/auth/social.php
@@ -1,13 +1,18 @@
 <?if (Theme::get('premium')==1):?>
-    <fieldset>
-        <?$providers = Social::get_providers(); $i = 0; foreach ($providers as $key => $value):?>
-            <?if($value['enabled']):?>
-                <?if($i==0):?>
-                    <legend><?=__('Social Login')?></legend>
-                <?endif?>
-                <?if(strtolower($key) == 'live')$key='windows'?>
-                    <a class="zocial <?=strtolower($key)?> social-btn" href="<?=Route::url('default',array('controller'=>'social','action'=>'login','id'=>strtolower($key)))?>"><?=$key?></a>
-            <?$i++; endif?>
-        <?endforeach?>
-    </fieldset>
+    <?if (count($providers = Social::enabled_providers()) > 0) :?>
+        <br><br>
+        <fieldset>
+            <legend><?=__('Social Login')?></legend>
+            <ul class="list-inline social-providers">
+                <?foreach ($providers as $key => $provider) :?>     
+                    <?if(strtolower($key) == 'live')$key='windows'?>
+                    <li>
+                        <a class="zocial <?=strtolower($key)?> social-btn" href="<?=Route::url('default',array('controller'=>'social','action'=>'login','id'=>strtolower($key)))?>">
+                            <?=$key?>
+                        </a>
+                    </li>
+                <?endforeach?>
+            </ul>
+        </fieldset>
+    <?endif?>
 <?endif?>

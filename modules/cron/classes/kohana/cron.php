3e31e9daa6ff262f8e9af82772bb043783e5ed92
chema <neo22s@gmail.com>
2016-01-14 12:55:22 +0100

    full columns https://github.com/open-classifieds/openclassifieds2/issues/1697

--- modules/cron/classes/kohana/cron.php
+++ modules/cron/classes/kohana/cron.php
@@ -32,22 +32,22 @@ class Kohana_Cron extends ORM {
     /**
      * Seconds whn we launch the cron again if didnt finish, this should be the maximum any of your crons takes to be executed.
      */
-    const TRESHOLD         = 360;
+    const TRESHOLD         = 600;
 
     /**
      * separator used in the table field params to separate the params we pass to the callback function.
      */
     const PARAMS_SEPARATOR = '|';
-	
+    
     /**
      * gets all the crons from crontab and executes them
      * @return string info
      */
-	public static function run()
-	{
+    public static function run()
+    {
         $time_start = microtime(true);
 
-		//get active crons and due to execute now or next execute is NULL
+        //get active crons and due to execute now or next execute is NULL
         $crontab = new Cron();
         $crontab = $crontab->where('active','=',1)
                             ->where_open()
@@ -70,16 +70,16 @@ class Kohana_Cron extends ORM {
         $seconds = microtime(true) - $time_start;
         
         return sprintf('Executed %d cronjobs in %d seconds',$crons_executed,$seconds);
-	}
+    }
 
 
 
-	/**
-	 * Execute this cron job
+    /**
+     * Execute this cron job
      * @return bool/mixed false if fails, string with func output 
-	 */
-	public function execute()
-	{
+     */
+    public function execute()
+    {
         $return = FALSE;
 
         if ($this->loaded())
@@ -113,7 +113,7 @@ class Kohana_Cron extends ORM {
 
         return $return;
         
-	}
+    }
 
     /**
      * executes the cron functions from the callback
@@ -128,10 +128,16 @@ class Kohana_Cron extends ORM {
             if (Cron::function_exists($this->callback))
             {
                 $params = explode(self::PARAMS_SEPARATOR, $this->params);
-                if (is_array($params))
-                    $return = call_user_func_array($this->callback,$params);
-                else
-                    $return = call_user_func($this->callback);
+
+                try {
+                    if (is_array($params))
+                        $return = call_user_func_array($this->callback,$params);
+                    else
+                        $return = call_user_func($this->callback);
+                } catch (Exception $e) {
+                    Kohana::$log->add(Log::ERROR,'Cron - '.$this->callback.' - '.print_r($e->getMessage(),1) );
+                }
+                
             }
             else
             {
@@ -189,6 +195,213 @@ class Kohana_Cron extends ORM {
     
     }
 
-
-
+protected $_table_columns =  
+array (
+  'id_crontab' => 
+  array (
+    'type' => 'int',
+    'min' => '0',
+    'max' => '4294967295',
+    'column_name' => 'id_crontab',
+    'column_default' => NULL,
+    'data_type' => 'int unsigned',
+    'is_nullable' => false,
+    'ordinal_position' => 1,
+    'display' => '10',
+    'comment' => '',
+    'extra' => 'auto_increment',
+    'key' => 'PRI',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'name' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'name',
+    'column_default' => NULL,
+    'data_type' => 'varchar',
+    'is_nullable' => false,
+    'ordinal_position' => 2,
+    'character_maximum_length' => '50',
+    'collation_name' => 'utf8_general_ci',
+    'comment' => '',
+    'extra' => '',
+    'key' => 'UNI',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'period' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'period',
+    'column_default' => NULL,
+    'data_type' => 'varchar',
+    'is_nullable' => false,
+    'ordinal_position' => 3,
+    'character_maximum_length' => '50',
+    'collation_name' => 'utf8_general_ci',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'callback' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'callback',
+    'column_default' => NULL,
+    'data_type' => 'varchar',
+    'is_nullable' => false,
+    'ordinal_position' => 4,
+    'character_maximum_length' => '140',
+    'collation_name' => 'utf8_general_ci',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'params' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'params',
+    'column_default' => NULL,
+    'data_type' => 'varchar',
+    'is_nullable' => true,
+    'ordinal_position' => 5,
+    'character_maximum_length' => '255',
+    'collation_name' => 'utf8_general_ci',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'description' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'description',
+    'column_default' => NULL,
+    'data_type' => 'varchar',
+    'is_nullable' => true,
+    'ordinal_position' => 6,
+    'character_maximum_length' => '255',
+    'collation_name' => 'utf8_general_ci',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'date_created' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'date_created',
+    'column_default' => 'CURRENT_TIMESTAMP',
+    'data_type' => 'timestamp',
+    'is_nullable' => false,
+    'ordinal_position' => 7,
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'date_started' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'date_started',
+    'column_default' => NULL,
+    'data_type' => 'datetime',
+    'is_nullable' => true,
+    'ordinal_position' => 8,
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'date_finished' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'date_finished',
+    'column_default' => NULL,
+    'data_type' => 'datetime',
+    'is_nullable' => true,
+    'ordinal_position' => 9,
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'date_next' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'date_next',
+    'column_default' => NULL,
+    'data_type' => 'datetime',
+    'is_nullable' => true,
+    'ordinal_position' => 10,
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'times_executed' => 
+  array (
+    'type' => 'int',
+    'min' => '-9223372036854775808',
+    'max' => '9223372036854775807',
+    'column_name' => 'times_executed',
+    'column_default' => '0',
+    'data_type' => 'bigint',
+    'is_nullable' => true,
+    'ordinal_position' => 11,
+    'display' => '20',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'output' => 
+  array (
+    'type' => 'string',
+    'column_name' => 'output',
+    'column_default' => NULL,
+    'data_type' => 'varchar',
+    'is_nullable' => true,
+    'ordinal_position' => 12,
+    'character_maximum_length' => '50',
+    'collation_name' => 'utf8_general_ci',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'running' => 
+  array (
+    'type' => 'int',
+    'min' => '-128',
+    'max' => '127',
+    'column_name' => 'running',
+    'column_default' => '0',
+    'data_type' => 'tinyint',
+    'is_nullable' => false,
+    'ordinal_position' => 13,
+    'display' => '1',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+  'active' => 
+  array (
+    'type' => 'int',
+    'min' => '-128',
+    'max' => '127',
+    'column_name' => 'active',
+    'column_default' => '1',
+    'data_type' => 'tinyint',
+    'is_nullable' => false,
+    'ordinal_position' => 14,
+    'display' => '1',
+    'comment' => '',
+    'extra' => '',
+    'key' => '',
+    'privileges' => 'select,insert,update,references',
+  ),
+);
 }

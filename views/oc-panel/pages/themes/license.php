b40497ca9be5c38ffd20ecfd776db2e279859271
Oliver <oliverds@users.noreply.github.com>
2016-05-17 10:01:42 -0500

    Fixes license view HTML markup

--- views/oc-panel/pages/themes/license.php
+++ views/oc-panel/pages/themes/license.php
@@ -10,25 +10,19 @@
         <form action="<?=URL::base()?><?=Request::current()->uri()?>" method="post"> 
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <div class="form-horizontal">
-                        <div class="form-group">
-                            <label class="control-label"><?=__('License')?></label>
-                            <div class="col-sm-4">
-                                <input class="form-control" type="text" name="license" value="" placeholder="<?=__('License')?>">
-                            </div>
-                          </div>
-                        <div class="form-actions">
-                            <button 
-                                type="button" 
-                                class="btn btn-primary submit" 
-                                title="<?=__('Are you sure?')?>" 
-                                data-text="<?=sprintf(__('License will be activated in %s domain. Once activated, your license cannot be changed to another domain.'), parse_url(URL::base(), PHP_URL_HOST))?>"
-                                data-btnOkLabel="<?=__('Yes, definitely!')?>" 
-                                data-btnCancelLabel="<?=__('No way!')?>">
-                                <?=__('Check')?>
-                            </button>
-                        </div>
+                    <div class="form-group">
+                        <label class="control-label"><?=__('License')?></label>
+                        <input class="form-control" type="text" name="license" value="" placeholder="<?=__('License')?>">
                     </div>
+                    <button 
+                        type="button" 
+                        class="btn btn-primary submit" 
+                        title="<?=__('Are you sure?')?>" 
+                        data-text="<?=sprintf(__('License will be activated in %s domain. Once activated, your license cannot be changed to another domain.'), parse_url(URL::base(), PHP_URL_HOST))?>"
+                        data-btnOkLabel="<?=__('Yes, definitely!')?>" 
+                        data-btnCancelLabel="<?=__('No way!')?>">
+                        <?=__('Check')?>
+                    </button>
                 </div>
             </div>
         </form>

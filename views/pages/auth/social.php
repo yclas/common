61b14d8e7ae3aba295fd607b0a1460cda5cded82
Oliver <oliverds@users.noreply.github.com>
2016-11-15 18:12:01 -0600

    closes yclas/yclas#1993: Live social auth error

--- views/pages/auth/social.php
+++ views/pages/auth/social.php
@@ -5,9 +5,8 @@
             <legend><?=_e('Social Login')?></legend>
             <ul class="list-inline social-providers">
                 <?foreach ($providers as $key => $provider) :?>     
-                    <?if(strtolower($key) == 'live')$key='windows'?>
                     <li>
-                        <a class="zocial <?=strtolower($key)?> social-btn" href="<?=Route::url('default',array('controller'=>'social','action'=>'login','id'=>strtolower($key)))?>">
+                        <a class="zocial <?=strtolower($key) == 'live' ? 'windows' : strtolower($key)?> social-btn" href="<?=Route::url('default',array('controller'=>'social','action'=>'login','id'=>strtolower($key)))?>">
                             <?=$key?>
                         </a>
                     </li>

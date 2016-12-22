3e09235eeda1b1a8e57e99fed10a1462c677a390
Oliver <oliverds@users.noreply.github.com>
2016-12-21 19:15:08 -0600

    Common for yclas/yclas#2201: Register better UX

--- views/pages/auth/login-form.php
+++ views/pages/auth/login-form.php
@@ -1,21 +1,22 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
+<?=View::factory('pages/auth/social')?>
 <form class="well form-horizontal auth" method="post" action="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'login'))?>">         
     <?=Form::errors()?>
     <div class="form-group">
-        <label class="col-sm-2 control-label"><?=_e('Email')?></label>
-        <div class="col-md-5 col-sm-6">
+        <label class="col-sm-4 control-label"><?=_e('Email')?></label>
+        <div class="col-sm-8">
             <input class="form-control" type="text" name="email" placeholder="<?=__('Email')?>">
         </div>
     </div>
      
     <div class="form-group">
-        <label class="col-sm-2 control-label"><?=_e('Password')?></label>
-        <div class="col-md-5 col-sm-6">
+        <label class="col-sm-4 control-label"><?=_e('Password')?></label>
+        <div class="col-sm-8">
             <input class="form-control" type="password" name="password" placeholder="<?=__('Password')?>">
             <p class="help-block">
-          		<small><a data-toggle="modal" data-dismiss="modal" href="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'forgot'))?>#forgot-modal">
-          			<?=_e('Forgot password?')?>
-          		</a></small>
+                <small><a data-toggle="modal" data-dismiss="modal" href="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'forgot'))?>#forgot-modal">
+                    <?=_e('Forgot password?')?>
+                </a></small>
             </p>
             <div class="checkbox">
                 <label>
@@ -24,16 +25,26 @@
             </div>
         </div>
     </div>
-    <div class="page-header"></div>     
-    <div class="col-sm-offset-2">
-        <button type="submit" class="btn btn-primary">
-            <i class="glyphicon glyphicon-user glyphicon"></i> <?=_e('Login')?>
-        </button>
-      	<a class="btn btn-default" data-toggle="modal" data-dismiss="modal" href="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'register'))?>#register-modal">
-        	<?=_e('Register')?>
-        </a>
+    
+    <hr>
+
+    <div class="form-group">
+        <div class="col-sm-offset-4 col-sm-8">
+            <ul class="list-inline">
+                <li>
+                    <button type="submit" class="btn btn-primary">
+                        <?=_e('Login')?>
+                    </button>
+                </li>
+                <li>
+                    <?=_e('Don\'t Have an Account?')?>
+                    <a data-toggle="modal" data-dismiss="modal" href="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'register'))?>#register-modal">
+                        <?=_e('Register')?>
+                    </a>
+                </li>
+            </ul>
+        </div>
     </div>
     <?=Form::redirect()?>
     <?=Form::CSRF('login')?>
-    <?=View::factory('pages/auth/social')?>
-</form>      	
+</form>         
--- views/pages/auth/register-form.php
+++ views/pages/auth/register-form.php
@@ -1,18 +1,18 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
+<?=View::factory('pages/auth/social')?>
 <form class="well form-horizontal register"  method="post" action="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'register'))?>">         
-          <?=Form::errors()?>
+    <?=Form::errors()?>
+    <div class="form-group">
+        <label class="col-sm-4 control-label"><?=_e('Name')?></label>
+        <div class="col-sm-8">
+            <input class="form-control" type="text" name="name" value="<?=Request::current()->post('name')?>" placeholder="<?=__('Name')?>">
+        </div>
+    </div>
           
-          <div class="form-group">
-            <label class="col-sm-2 control-label"><?=_e('Name')?></label>
-            <div class="col-md-5 col-sm-6">
-              <input class="form-control" type="text" name="name" value="<?=Request::current()->post('name')?>" placeholder="<?=__('Name')?>">
-            </div>
-          </div>
-          
-          <div class="form-group">
-            <label class="col-sm-2 control-label"><?=_e('Email')?></label>
-            <div class="col-md-5 col-sm-6">
-              <input
+    <div class="form-group">
+        <label class="col-sm-4 control-label"><?=_e('Email')?></label>
+        <div class="col-sm-8">
+            <input
                 class="form-control" 
                 type="text" 
                 name="email" 
@@ -20,64 +20,69 @@
                 placeholder="<?=__('Email')?>" 
                 data-domain='<?=(core::config('general.email_domains') != '') ? json_encode(explode(',', core::config('general.email_domains'))) : ''?>' 
                 data-error="<?=__('Email must contain a valid email domain')?>"
-              >
-            </div>
-          </div>
+            >
+        </div>
+    </div>
      
-          <div class="form-group">
-            <label class="col-sm-2 control-label"><?=_e('New password')?></label>
-            <div class="col-md-5 col-sm-6">
-            <input class="form-control" type="password" name="password1" placeholder="<?=__('Password')?>">
-            </div>
-          </div>
+    <div class="form-group">
+        <label class="col-sm-4 control-label"><?=_e('New password')?></label>
+        <div class="col-sm-8">
+            <input id="register_password" class="form-control" type="password" name="password1" placeholder="<?=__('Password')?>">
+        </div>
+    </div>
           
-          <div class="form-group">
-            <label class="col-sm-2 control-label"><?=_e('Repeat password')?></label>
-            <div class="col-md-5 col-sm-6">
+    <div class="form-group">
+        <label class="col-sm-4 control-label"><?=_e('Repeat password')?></label>
+        <div class="col-sm-8">
             <input class="form-control" type="password" name="password2" placeholder="<?=__('Password')?>">
-              <p class="help-block">
-              		<?=_e('Type your password twice')?>
-              </p>
-            </div>
-          </div>
-          <?if(core::config('advertisement.tos') != ''):?>
-            <div class="form-group">
-              <div class="col-md-5 col-sm-6 col-sm-offset-2">
+            <p class="help-block">
+                <?=_e('Type your password twice')?>
+            </p>
+        </div>
+    </div>
+
+    <?if(core::config('advertisement.tos') != ''):?>
+        <div class="form-group">
+            <div class="col-sm-8 col-sm-offset-4">
                 <div class="checkbox">
-                  <label>
-                      <input type="checkbox" required name="tos" id="tos"/> 
-                    <a target="_blank" href="<?=Route::url('page', array('seotitle'=>core::config('advertisement.tos')))?>"> <?=_e('Terms of service')?></a>
-                  </label>
+                    <label>
+                        <input type="checkbox" required name="tos" id="tos"/> 
+                        <a target="_blank" href="<?=Route::url('page', array('seotitle'=>core::config('advertisement.tos')))?>"> <?=_e('Terms of service')?></a>
+                    </label>
                 </div>
-              </div>
             </div>
-          <?endif?>
-          <div class="form-group">
-            <?if (core::config('advertisement.captcha') != FALSE OR core::config('general.captcha') != FALSE):?>
-              <?if (Core::config('general.recaptcha_active')):?>
-                <div class="col-sm-2"></div>
-                <div class="col-md-5 col-sm-6">
-                  <?=Captcha::recaptcha_display()?> 
-                  <div id="<?=isset($recaptcha_placeholder) ? $recaptcha_placeholder : 'recaptcha3'?>"></div>
+        </div>
+    <?endif?>
+    
+    <div class="form-group">
+        <?if (core::config('advertisement.captcha') != FALSE OR core::config('general.captcha') != FALSE):?>
+            <?if (Core::config('general.recaptcha_active')):?>
+                <div class="col-sm-4"></div>
+                <div class="col-sm-8">
+                    <?=Captcha::recaptcha_display()?> 
+                    <div id="<?=isset($recaptcha_placeholder) ? $recaptcha_placeholder : 'recaptcha3'?>"></div>
                 </div>
-              <?else:?>
-                <label class="col-sm-2 control-label"><?=_e('Captcha')?>*:</label>
-                <div class="col-md-5 col-sm-6">
-                  <span id="helpBlock" class="help-block"><?=captcha::image_tag('register')?></span>
-                  <?= FORM::input('captcha', "", array('class' => 'form-control', 'id' => 'captcha', 'required', 'data-error' => __('Captcha is not correct')))?>
+            <?else:?>
+                <label class="col-sm-4 control-label"><?=_e('Captcha')?>*:</label>
+                <div class="col-sm-8">
+                    <span id="helpBlock" class="help-block"><?=captcha::image_tag('register')?></span>
+                    <?= FORM::input('captcha', "", array('class' => 'form-control', 'id' => 'captcha', 'required', 'data-error' => __('Captcha is not correct')))?>
                 </div>
-              <?endif?>
             <?endif?>
-          </div>
-            <div class="page-header"></div> 
-            <div class="col-sm-offset-2">
+        <?endif?>
+    </div>
+    
+    <hr>
+    
+    <div class="form-group">
+        <div class="col-sm-offset-4 col-sm-8">
             <button type="submit" class="btn btn-primary"><?=_e('Register')?></button>
-              	<a class="btn btn-default"  data-dismiss="modal" data-toggle="modal"  href="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'login'))?>#login-modal">
-    				<i class="glyphicon glyphicon-user"></i> 
-    				<?=_e('Login')?>
-    			</a>
-            </div>
-          <?=Form::redirect()?>
-          <?=Form::CSRF('register')?>
-          <?=View::factory('pages/auth/social')?>
-</form>      	
+            <a class="btn btn-default"  data-dismiss="modal" data-toggle="modal"  href="<?=Route::url('oc-panel',array('directory'=>'user','controller'=>'auth','action'=>'login'))?>#login-modal">
+                <i class="glyphicon glyphicon-user"></i> 
+                <?=_e('Login')?>
+            </a>
+        </div>
+    </div>
+    <?=Form::redirect()?>
+    <?=Form::CSRF('register')?>
+</form>         

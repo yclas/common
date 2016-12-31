91cd2d77a3139fdd9cc785222a65c44f43e52766
chema <neo22s@gmail.com>
2016-12-31 08:38:34 +0100

    url error

--- views/oc-panel/pages/update/confirm.php
+++ views/oc-panel/pages/update/confirm.php
@@ -21,7 +21,7 @@
         <ul>
             <li><?=__('Backup all your files and database')?>. <a target="_blank" href="https://docs.yclas.com/backup-classifieds-site/"><?=__('Read more')?></a></li>
             <li><?=__('This process can take few minutes DO NOT interrupt it')?></li>
-            <li><?=__('If you have doubts check the release notes for this version')?>. <a target="_blank" href="<?=$version['blog']?>"><?=__('Release Notes')?> <?=$latest_version?></a></li>
+            <li><?=__('If you have doubts check the release notes for this version')?>. <a target="_blank" href="<?=$version['changelog']?>"><?=__('Release Notes')?> <?=$latest_version?></a></li>
             <li><?=__('You are responsible for any damages or down time at your site')?></li>
         </ul>
     </p>
